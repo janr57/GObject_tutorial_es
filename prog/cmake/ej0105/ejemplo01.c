886362b4faa2084c09cdcf481a8a89f4a535e4c3
janr <janr.devel@gmail.com>
2024-03-29 15:47:29 +0100

    Se añaden diferentes métodos de compilación de los ejemplos
    Se utilizan: make, meson y cmake.

---
+++ prog/cmake/ej0105/ejemplo01.c
@@ -0,0 +1,38 @@
+/*
+* ejemplo01.c
+* Ejemplo de creación y destrucción de instancias de 'GObjectClass'.
+ * https://github.com/ToshioCP/Gobject-tutorial/blob/main/gfm/sec2.md
+ * https://github.com/ToshioCP/Gobject-tutorial/tree/main
+ */
+
+#include <locale.h>
+#include <glib-object.h>
+
+int
+main (int argc, char **argv)
+{
+  GObject *instancia1, *instancia2;
+  GObjectClass *clase1, *clase2;
+
+  // Para que g_print no transforme la codificación de caracteres
+  // por su cuenta y no se vean los acentos.
+  setlocale(LC_CTYPE, "");
+
+  // Creación de dos instancias de la clase 'GObject'
+  instancia1 = g_object_new (G_TYPE_OBJECT,  NULL);
+  instancia2 = g_object_new (G_TYPE_OBJECT, NULL);
+  g_print ("Dirección de 'instancia1': %p\n", instancia1);
+  g_print ("Dirección de 'instancia2': %p\n", instancia2);
+
+  // Comprobación de que solo hay una clase 'GObject'
+  clase1 = G_OBJECT_GET_CLASS (instancia1);
+  clase2 = G_OBJECT_GET_CLASS (instancia2);
+  g_print("Dirección de la clase de 'instancia1': %p\n", clase1);
+  g_print("Dirección de la clase de 'instancia2': %p\n", clase2);
+
+  // Liberación de las instancias
+  g_object_unref(instancia1);
+  g_object_unref(instancia2);
+
+  return 0;
+}
---
+++ prog/meson/ej0105/ejemplo02.c
@@ -0,0 +1,58 @@
+/*
+ * ejemplo02.c
+ * Prueba de las referencias a una instancia.
+ */
+
+#include <locale.h>
+#include <glib-object.h>
+
+static void
+show_ref_count  (GObject *instancia)
+{
+  if  (G_IS_OBJECT (instancia)) {
+    /* Los usuarios no deberían utilizar el método 'ref_count' en sus programas.
+       pues interferirían de una forma impredecible con GObject */
+    /* Aquí se utiliza solo como demostración */
+    g_print ("  La cuenta de referencia de la instancia es %d.\n", instancia->ref_count);
+  }
+  else {
+    g_print ("  La instancia recibida no es un objeto de 'GObjectClass'.\n");
+  }
+}
+
+int
+main (int argc, char **argv)
+{
+  GObject *instancia;
+
+  // Para que g_print no transforme la codificación de caracteres
+  // por su cuenta y no se vean los acentos.
+  setlocale(LC_CTYPE, "");
+
+  g_print ("- Se crea el objeto 'instancia', llamando a 'g_object_new'.\n");
+  instancia = g_object_new(G_TYPE_OBJECT, NULL);
+  show_ref_count (instancia);
+  g_print("\n");
+  
+  g_print ("- Se añade una referencia a 'instancia', llamando a 'g_object_ref'.\n");
+  g_object_ref (instancia);
+  show_ref_count (instancia);
+  g_print("\n");
+
+  g_print ("- Se elimina una referencia, llamando a 'g_object_unref'.\n");
+  g_object_unref(instancia);
+  show_ref_count (instancia);
+   g_print("\n");
+
+  g_print ("- Se elimina la última referencia, llamando a 'g_object_unref'.\n");
+  g_object_unref (instancia);
+  g_print ("  La cuenta de referencia vale cero y la instancia se destruye.\n");
+  g_print ("  Su memoria asociada se libera.\n");
+  g_print("  Una llamada a 'show_ref_count(instance)' daría un error, pues la\n");
+  g_print("  instancia  ya no existe.\n");
+  g_print("  Por ello, se ha comentado esta línea en el código fuente.\n");
+  /* show_ref_count (instancia); */
+  
+  return 0;
+}
+
