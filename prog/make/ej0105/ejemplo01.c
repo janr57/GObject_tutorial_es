5bb9cbbf7e6ebe86f1ac03bc987711a2382c73f1
janr <janr.devel@gmail.com>
2024-03-29 18:54:57 +0100

    Pequeñas correcciones

--- prog/make/ej0105/ejemplo01.c
+++ prog/make/ej0105/ejemplo01.c
@@ -14,23 +14,23 @@ main (int argc, char **argv)
   GObject *instancia1, *instancia2;
   GObjectClass *clase1, *clase2;
 
-  // Para que g_print no transforme la codificación de caracteres
-  // por su cuenta y no se vean los acentos.
+  /* Para que g_print no transforme la codificación de caracteres
+     por su cuenta y no se vean los acentos */
   setlocale(LC_CTYPE, "");
 
-  // Creación de dos instancias de la clase 'GObject'
+  /* Creación de dos instancias de la clase 'GObject' */
   instancia1 = g_object_new (G_TYPE_OBJECT,  NULL);
   instancia2 = g_object_new (G_TYPE_OBJECT, NULL);
   g_print ("Dirección de 'instancia1': %p\n", instancia1);
   g_print ("Dirección de 'instancia2': %p\n", instancia2);
 
-  // Comprobación de que solo hay una clase 'GObject'
+  /* Comprobación de que las dos instancias pertenecen a la misma clase */
   clase1 = G_OBJECT_GET_CLASS (instancia1);
   clase2 = G_OBJECT_GET_CLASS (instancia2);
   g_print("Dirección de la clase de 'instancia1': %p\n", clase1);
   g_print("Dirección de la clase de 'instancia2': %p\n", clase2);
 
-  // Liberación de las instancias
+  /* Destrucción de las instancias */
   g_object_unref(instancia1);
   g_object_unref(instancia2);
 
